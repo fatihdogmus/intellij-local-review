e29014e770563404b07160c0001cfb729bc87435
GitHub Action <action@github.com>
2026-05-07 06:42:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/fatihdogmus/intellijlocalreview/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/fatihdogmus/intellijlocalreview/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.fatihdogmus.intellijlocalreview.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
